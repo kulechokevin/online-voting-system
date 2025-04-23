d23a8f35409e88223fb145b880f25bb16e1178ab
kulechokevin <kulechokevin02@gmail.com>
2025-04-23 16:41:14 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2025. Developed by Kelvin Barasa : IN14/00029/19.
+<br/>
+Online Voting System.
+<br/>
+Final Year Project 2025
+<br/>
+Contact: <a href="tel:+254713176817">+254713176817</a> | Email: <a href="mailto:kulechokevin02@gmail.com">kulechokevin02@gmail.com</a>
+
+</footer>
